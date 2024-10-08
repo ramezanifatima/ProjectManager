1e0886f744612972e4e5893822f8f81a539dbd68
ramezanifatima <ramezanifatemeh988@gmail.com>
2024-10-08 09:45:12 -0700

    add crud Operation from task and subtask

--- Projects/urls.py
+++ Projects/urls.py
@@ -1,11 +1,23 @@
 from django.urls import path
-from .views import ProjectListView,ProjectCreateView,UpdateProjectView
+from .views import ProjectListView, ProjectCreateView, ProjectDeleteView, UpdateProjectView, DetailProjectView, \
+    DetailTaskView, CreateTaskView, TaskDeleteView, UpdateTaskView, DetailSubTaskView, SubTaskDeleteView, \
+    CreateSubTaskView, UpdateSubTaskView
 
 app_name = 'projects'
 
 urlpatterns = [
     path('', ProjectListView.as_view(), name='projects_list'),
     path('create', ProjectCreateView.as_view(), name='projects_create'),
-    path('delete/project/<int:pk>', ProjectCreateView.as_view(), name='projects_create'),
+    path('delete/project/<int:pk>', ProjectDeleteView.as_view(), name='projects_delete'),
     path('update/project/<int:pk>', UpdateProjectView.as_view(), name='projects_update'),
+    path('project/<int:pk>', DetailProjectView.as_view(), name='projects_detail'),
+    path('project/task/<int:pk>', DetailTaskView.as_view(), name='task_detail'),
+    path('project/<int:pk_project>/create/task', CreateTaskView.as_view(), name='create_task'),
+    path('delete/task/<int:pk>', TaskDeleteView.as_view(), name='task_delete'),
+    path('update/task/<int:pk>', UpdateTaskView.as_view(), name='task_update'),
+    path('project/task/subtask/<int:pk>', DetailSubTaskView.as_view(), name='subtask_detail'),
+    path('task/<int:pk_task>/create/subtask', CreateSubTaskView.as_view(), name='create_subtask'),
+    path('delete/subtask/<int:pk>', SubTaskDeleteView.as_view(), name='subtask_delete'),
+    path('update/subtask/<int:pk>', UpdateSubTaskView.as_view(), name='subtask_update'),
+
 ]
--- Projects/views.py
+++ Projects/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
-from django.views.generic import ListView, CreateView, DeleteView, UpdateView
-from .models import Project
-from .forms import ProjectForm, UpdateForm
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView, DetailView
+from .models import Project, Task, SubTask
+from .forms import ProjectForm, UpdateProjectForm, TaskForm, TaskUpdateForm, SubTaskForm, SubTaskUpdateForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
+from django.urls import reverse
 
 
 class ProjectListView(LoginRequiredMixin, ListView):
@@ -22,7 +23,7 @@ class ProjectListView(LoginRequiredMixin, ListView):
 
 class ProjectCreateView(LoginRequiredMixin, CreateView):
     """
-    this view
+    This view is used to create a new project
     """
     form_class = ProjectForm
     template_name = 'Projects/create_project.html'
@@ -50,15 +51,8 @@ class ProjectCreateView(LoginRequiredMixin, CreateView):
 
 class ProjectDeleteView(DeleteView):
     """
-
+    This view is used to delete a project
     """
-    model = Project
-    pk_url_kwarg = 'pk'
-    template_name = 'Projects/delete_project.html'
-    success_url = '/'
-
-
-class UpdateProjectUpdate(UpdateView):
     model = Project
     pk_url_kwarg = 'pk'
     template_name = 'Projects/delete_project.html'
@@ -67,10 +61,129 @@ class UpdateProjectUpdate(UpdateView):
 
 class UpdateProjectView(UpdateView):
     """
-
+    This view is used to edit a project
     """
     model = Project
-    form_class = UpdateForm
     pk_url_kwarg = 'pk'
+    form_class = UpdateProjectForm
     template_name = 'Projects/update.html'
     success_url = '/'
+
+
+class DetailProjectView(DetailView):
+    """
+    This view returns the details of a project
+    """
+    model = Project
+    pk_url_kwarg = 'pk'
+    template_name = 'Projects/mor_info_project.html'
+
+
+class DetailTaskView(DetailView):
+    """
+    This view returns the details of a task and show mor subtasks
+    """
+    model = Task
+    template_name = 'Projects/mor_info_task.html'
+
+
+class CreateTaskView(CreateView):
+    """
+
+    """
+    form_class = TaskForm
+    template_name = 'Projects/create_task.html'
+    pk_url_kwarg = 'pk_project'
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST, request.FILES)
+        pk_project = self.kwargs.get(self.pk_url_kwarg)
+        project = get_object_or_404(Project, pk=pk_project)
+
+        if form.is_valid():
+            data = form.cleaned_data
+            print(data)
+            task = Task.objects.create(
+                project=project, title=data['title'],
+                description=data['description'], color=data['color'],
+                image=data['image'], start_date=data['start_date'],
+                end_date=data['end_date'], budget=data['budget'],
+            )
+            if task:
+                print('task')
+                messages.success(request, 'your task saved ')
+                return redirect(reverse('projects:projects_detail', kwargs={'pk': pk_project}))
+            messages.error(request, 'something went wrong', 'danger')
+        else:
+            print(form.errors)
+            form = self.form_class()
+            return render(request, self.template_name, {'form': form})
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    pk_url_kwarg = 'pk'
+    template_name = 'Projects/delete_task.html'
+    success_url = '/'
+
+
+class UpdateTaskView(UpdateView):
+    model = Task
+    pk_url_kwarg = 'pk'
+    form_class = TaskUpdateForm
+    template_name = 'Projects/update_task.html'
+    success_url = '/'
+
+
+class CreateSubTaskView(CreateView):
+    """
+
+    """
+    form_class = SubTaskForm
+    template_name = 'Projects/create_sub_task.html'
+    pk_url_kwarg = 'pk_task'
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST, request.FILES)
+        pk_task = self.kwargs.get(self.pk_url_kwarg)
+        task = get_object_or_404(Task, pk=pk_task)
+        project = get_object_or_404(Project,task=task)
+
+        if form.is_valid():
+            data = form.cleaned_data
+            task = SubTask.objects.create(
+                task=task, title=data['title'],
+                description=data['description'], color=data['color'],
+                image=data['image'], start_date=data['start_date'],
+                end_date=data['end_date'], budget=data['budget'],
+            )
+            if task:
+                messages.success(request, 'your task saved ')
+                return redirect(reverse('projects:projects_detail', kwargs={'pk': project.id}))
+            messages.error(request, 'something went wrong', 'danger')
+        else:
+            form = self.form_class()
+            return render(request, self.template_name, {'form': form})
+
+
+class SubTaskDeleteView(DeleteView):
+    model = SubTask
+    pk_url_kwarg = 'pk'
+    template_name = 'Projects/delete_subtask.html'
+    success_url = '/'
+
+
+class UpdateSubTaskView(UpdateView):
+    model = SubTask
+    pk_url_kwarg = 'pk'
+    form_class = SubTaskUpdateForm
+    template_name = 'Projects/update_subtask.html'
+    success_url = '/'
+
+
+class DetailSubTaskView(DetailView):
+    """
+    This view returns the details of a subtask
+    """
+    model = SubTask
+    template_name = 'Projects/mor_info_task.html'
